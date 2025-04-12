c48864328edbe3bf1309deca82ad03bd70dd7aed
BrightStackDev <brightstack.work.01@gmail.com>
2025-04-12 19:29:57 +0530

    Update userDetails.js

--- src/APIs/userDetails.js
+++ src/APIs/userDetails.js
@@ -2,18 +2,28 @@ import axios from "axios";
 
 export const getUserDetails = async () => {
     try {
+        console.log("Fetching user details from:", import.meta.env.VITE_SERVER_URL);
         const response = await axios.get(`${import.meta.env.VITE_SERVER_URL}/api/getUserDetails`, {
             withCredentials: true,
+            headers: {
+                'Accept': 'application/json',
+                'Content-Type': 'application/json'
+            }
         });
-        console.log(response);
-        // No need to return anything
-        if(response.data === "") {
+        
+        console.log("User details response:", response);
+        
+        if (!response.data || response.data === "") {
+            console.log("No user data returned, redirecting to login");
             window.location.href = "/login";
             return null;
         }
         return response.data;
     } catch (err) {
-        console.error(err);
-        return null; // Return null in case of error
+        console.error("Error fetching user details:", err);
+        if (err.response && err.response.status === 401) {
+            window.location.href = "/login";
+        }
+        return null;
     }
 };
