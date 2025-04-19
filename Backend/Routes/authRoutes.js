a61e165905b5ea49473dc115dbce7b6dc9d525f3
BrightStackDev <brightstack.work.01@gmail.com>
2025-04-20 00:16:00 +0530

    Update authRoutes.js

--- Backend/Routes/authRoutes.js
+++ Backend/Routes/authRoutes.js
@@ -1,9 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const { register, login, confirmEmail } = require('../Controllers/authController');
+const { register, login, confirmEmail, checkEmail, requestPasswordReset, resetPassword } = require('../Controllers/authController');
 
 router.post('/register', register);
 router.post('/login', login);
 router.get('/confirmation/:token', confirmEmail);
+router.get('/checkEmail', checkEmail);
+router.post('/requestPasswordReset', requestPasswordReset);
+router.post('/resetPassword', resetPassword);
 
 module.exports = router;
